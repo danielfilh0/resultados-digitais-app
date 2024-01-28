65b841482168ba60f373019bce0bfed394ba03e1
Daniel Filho <danbsilva98@gmail.com>
2024-01-28 00:43:47 -0300

    feat: add data handler on Result page

--- src/templates/Home/useHomeController.ts
+++ src/templates/Home/useHomeController.ts
@@ -1,25 +1,12 @@
 'use client'
 
-import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
-
-const schema = z.object({
-  name: z.string().min(2, 'O nome deve ter pelo menos 2 caracteres'),
-  email: z.string().min(1, 'E-mail é obrigatório').email('E-mail não é válido'),
-  phone: z
-    .string()
-    .length(16, 'Telefone inválido')
-    .transform((val) => val.trim())
-})
-
-export type FormData = z.infer<typeof schema> & {
-  [key: string]: string
-}
+import { FormSchemaData, formSchema } from '@/utils/form-schema'
 
 export type useHomeControllerProps = {
-  onSubmit?: (data: FormData) => void
+  onSubmit?: (data: FormSchemaData) => void
 }
 
 export function useHomeController({ onSubmit }: useHomeControllerProps) {
@@ -29,8 +16,8 @@ export function useHomeController({ onSubmit }: useHomeControllerProps) {
     register,
     handleSubmit: hookFormSubmit,
     formState: { errors }
-  } = useForm<FormData>({
-    resolver: zodResolver(schema)
+  } = useForm<FormSchemaData>({
+    resolver: zodResolver(formSchema)
   })
 
   const handleSubmit = hookFormSubmit(async (data) => {
--- src/templates/Result/test.tsx
+++ src/templates/Result/test.tsx
@@ -5,12 +5,27 @@ import { renderWithTheme } from '@/utils/tests/helpers'
 
 describe('<Result />', () => {
   it('should render the card download button', () => {
-    const { container } = renderWithTheme(<Result />)
+    const { container } = renderWithTheme(
+      <Result
+        name="Example Name"
+        email="emailexample@example.com"
+        phone="(99) 9 9999-9999"
+      />
+    )
 
     expect(
       screen.getByRole('button', { name: /baixar cartão/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should show error if the card data are inconsistent', () => {
+    renderWithTheme(<Result name="" email="wrongemail" phone="12345" />)
+
+    expect(screen.getByText(/erro/i)).toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', { name: /baixar cartão/i })
+    ).not.toBeInTheDocument()
   })
 })
